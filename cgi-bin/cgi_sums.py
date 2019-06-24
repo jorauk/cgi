40eb67969f6dee6547626d21a9cdee1c7e102f81
Jonathan Mauk <jorauk@uw.edu>
2019-06-24 13:42:44 -0700

    Third attempt, with some code to clean up the formatting.

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -6,9 +6,10 @@ cgitb.enable()
 
 form = cgi.FieldStorage()
 operands = form.getlist('operand')
-total = sum(map(float, operands))
+total = sum(map(int, operands))
 
 print("Content-type: text/plain")
 print()
-print("The numbers you are attempting to add: {}".format(str(operands)))
+print("The numbers you are attempting to add: ", end="")
+print(*operands, sep=" + ")
 print("The sum is: {}".format(str(total)))
