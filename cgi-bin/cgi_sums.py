f524f0aef6c95c378a3582e5ff86ff2286114b8a
Jonathan Mauk <jorauk@uw.edu>
2019-06-24 13:12:52 -0700

    First stab at sums exercise. Not implementing any addition code yet, just making sure I understand how FieldStorage works.

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,9 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+listval = form.getlist('operand')
+
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print("The numbers: {}".format(str(listval)))
